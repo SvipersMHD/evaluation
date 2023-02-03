d7eba2ed6886fa2babd7a26597687d5f0db7d139
SvipersMHD <zohaibadnan29.10@gmail.com>
2023-02-03 09:07:02 +0100

    fin

--- js/main.js
+++ js/main.js
@@ -1,8 +1,24 @@
 var burger = document.querySelector(".burger-menu");
-var nav = document.querySelector("nav");
+var burgerRotateHaut = document.querySelector(".bar__haut");
+var burgerRotateCentre = document.querySelector(".bar__centre");
+var burgerRotateBas = document.querySelector(".bar__bas");
+var navColor = document.querySelectorAll(".nav__a a");
 var navTranslate = document.querySelector(".nav__a");
 
+
+
 burger.addEventListener("click", function(){
-   nav.classList.toggle("__is-Active");
+   navTranslate.classList.toggle("__is-Active");
+   burgerRotateHaut.classList.toggle("rotation__haut");
+   burgerRotateCentre.classList.toggle("rotation__centre");
+   burgerRotateBas.classList.toggle("rotation__bas");
+
    console.log("test");
+})
+
+navColor.forEach(function(nav) {
+   burger.addEventListener("click", function(){
+      nav.classList.toggle("__a__color");
+      console.log("second");
+   })
 })
